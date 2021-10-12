2fce299f69d696acebdb7c99fc5363735286d519
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-10-12 20:22:39 +0900

    mysite02

---
+++ mysite02/src/main/java/com/douzone/mysite/mvc/guestbook/GuestbookActionFactory.java
@@ -0,0 +1,24 @@
+package com.douzone.mysite.mvc.guestbook;
+
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
+
+public class GuestbookActionFactory extends ActionFactory {
+
+	@Override
+	public Action getAction(String actionName) {
+		Action action = null;
+		System.out.println(actionName);
+		if ("writeguestbook".equals(actionName)) {
+			action = new WriteGuestbookAction();
+		} else if ("deleteform".equals(actionName)) {
+			action = new DeleteFormAction();
+		} else {
+			action = new ListAction();
+		}
+		
+		
+		return action;
+	}
+
+}
