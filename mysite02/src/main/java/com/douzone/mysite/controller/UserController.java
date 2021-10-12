347e483245510a87ce12b85fc77bb03c9634eaf6
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-10-12 12:29:28 +0900

    init

---
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.douzone.mysite.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.douzone.web.util.MvcUtil;
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("a");
+		
+		if("joinform".equals(action)) {
+			MvcUtil.forward("WEB-INF/views/user/joinform.jsp", request, response);
+		} else if ("join".equals(action)) {
+			
+		} else {
+			MvcUtil.redirect(request.getContextPath(), request, response);
+		}
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
