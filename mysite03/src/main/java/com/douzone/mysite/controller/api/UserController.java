eadecf6fe7d58ce08249bebdf691c6ae7f8c0df1
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-10-25 17:01:49 +0900

    ajax #2 finish

--- mysite03/src/main/java/com/douzone/mysite/controller/api/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/api/UserController.java
@@ -1,32 +1,34 @@
 package com.douzone.mysite.controller.api;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.douzone.mysite.dto.JsonResult;
 import com.douzone.mysite.service.UserService;
 import com.douzone.mysite.vo.UserVo;
 
-@Controller("userApiController")
+@RestController("userApiController")
 @RequestMapping("/user/api")
 public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@ResponseBody
-	@RequestMapping("/checkemail")
-	public Map<String, Object> checkEmail(
+//	@ResponseBody
+//	@RequestMapping("/checkemail")
+	@GetMapping("/checkemail")
+	public JsonResult checkEmail(
 		@RequestParam(value="email", required=true, defaultValue="") String email) {
 		UserVo userVo = userService.getUser(email);
 		
-		Map<String, Object> map = new HashMap<>();
-		map.put("exist", userVo != null);
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("result", "success");
+//		map.put("data", userVo != null);
+//		map.put("message", null);
 		
-		return map;
+		
+		return JsonResult.success(userVo != null);
 	}
 }
